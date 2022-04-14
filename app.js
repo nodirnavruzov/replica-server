bb4684cd3c8580ced55ffc70517dd354b450fc06
Nodir Navruzov <spy.nowin@gmail.com>
2022-04-14 14:23:25 +0500

    refactored morgan-logger

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const logger = require('./utils/logger')
 var indexRouter = require("./routes");
 
 var app = express();
 var cors = require("cors");
 
 
-app.use(logger("dev"));
+app.use(logger('myformat'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
